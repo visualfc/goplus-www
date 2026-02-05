305c606e67ad14cf18b972e90556d4f0352ff3a7
七叶 <visualfc@gmail.com>
2026-02-05 15:26:53 +0800

    Update playground/sandbox.go
    Co-authored-by: Aofei Sheng <aofei@aofeisheng.com>

--- playground/sandbox.go
+++ playground/sandbox.go
@@ -500,7 +500,6 @@ import (
 		if err := os.MkdirAll(filepath.Join(tmpDir, "dummy"), 0755); err != nil {
 			return nil, fmt.Errorf("failed to create dummy directory: %w", err)
 		}
-		dumyCmd.Run()
 		if err := os.WriteFile(filepath.Join(tmpDir, "dummy", "dummy.go"), []byte(dummy), 0644); err != nil {
 			return nil, fmt.Errorf("failed to write dummy.go: %w", err)
 		}
