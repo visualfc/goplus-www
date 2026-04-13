8a8068c66813cb1747f2e1dab50e89c2aa51bf0d
visualfc <visualfc@gmail.com>
2026-04-13 19:54:11 +0800

    playground: formatCode

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/goplus/www
 
-go 1.12
+go 1.24.0
 
 require (
 	cloud.google.com/go v0.75.0
@@ -9,15 +9,48 @@ require (
 	github.com/aws/aws-sdk-go v1.37.0
 	github.com/bradfitz/gomemcache v0.0.0-20190913173617-a41fca850d0b
 	github.com/google/go-cmp v0.5.5
-	github.com/goplus/mod v0.19.0
+	github.com/goplus/mod v0.20.2
+	github.com/goplus/xgo v1.7.1-0.20260413043704-93ff1eed8445
 	github.com/shurcooL/webdavfs v0.0.0-20190527155401-0680c3c63e3c
 	go.opencensus.io v0.23.0
 	golang.org/x/mod v0.20.0
-	golang.org/x/net v0.0.0-20210119194325-5f4716e94777
+	golang.org/x/net v0.50.0
 	golang.org/x/tools v0.1.7
 	google.golang.org/api v0.37.0
 	google.golang.org/genproto v0.0.0-20210126160654-44e461bb6506
+)
+
+require (
+	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
+	github.com/alecthomas/units v0.0.0-20190924025748-f65c72e2690d // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/census-instrumentation/opencensus-proto v0.3.0 // indirect
+	github.com/cespare/xxhash/v2 v2.1.1 // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/golang/protobuf v1.4.3 // indirect
+	github.com/googleapis/gax-go/v2 v2.0.5 // indirect
+	github.com/goplus/gogen v1.23.0-pre.2 // indirect
+	github.com/hashicorp/golang-lru v0.5.4 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/konsorten/go-windows-terminal-sequences v1.0.3 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
+	github.com/prometheus/client_golang v1.9.0 // indirect
+	github.com/prometheus/client_model v0.2.0 // indirect
+	github.com/prometheus/common v0.15.0 // indirect
+	github.com/prometheus/procfs v0.2.0 // indirect
+	github.com/prometheus/statsd_exporter v0.20.0 // indirect
+	github.com/qiniu/x v1.17.0 // indirect
+	github.com/sirupsen/logrus v1.6.0 // indirect
+	golang.org/x/oauth2 v0.0.0-20210126194326-f9ce19ea3013 // indirect
+	golang.org/x/sync v0.0.0-20200317015054-43a5402ce75a // indirect
+	golang.org/x/sys v0.41.0 // indirect
+	golang.org/x/text v0.3.5 // indirect
+	golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543 // indirect
+	google.golang.org/appengine v1.6.6 // indirect
 	google.golang.org/grpc v1.39.0 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
+	gopkg.in/alecthomas/kingpin.v2 v2.2.6 // indirect
+	gopkg.in/yaml.v2 v2.3.0 // indirect
 )
 
 replace (
--- playground/fmt.go
+++ playground/fmt.go
@@ -6,17 +6,13 @@ package main
 
 import (
 	"encoding/json"
-	"errors"
-	"fmt"
+	"go/format"
 	"net/http"
-	"os"
-	"os/exec"
 	"path"
-	"path/filepath"
-	"strings"
 
+	xmodfile "github.com/goplus/mod/modfile"
+	xformat "github.com/goplus/xgo/x/format"
 	"golang.org/x/mod/modfile"
-	"golang.org/x/tools/imports"
 )
 
 type fmtResponse struct {
@@ -32,72 +28,80 @@ func handleFmt(w http.ResponseWriter, r *http.Request) {
 	}
 	w.Header().Set("Content-Type", "application/json")
 
-	fs, err := splitFiles([]byte(r.FormValue("body")))
+	data, err := formatCode([]byte(r.FormValue("body")))
 	if err != nil {
 		json.NewEncoder(w).Encode(fmtResponse{Error: err.Error()})
 		return
 	}
-
-	fixImports := r.FormValue("imports") != ""
-	for _, f := range fs.files {
-		switch {
-		case path.Ext(f) == ".go":
-			var out []byte
-			var err error
-			in := fs.Data(f)
-			if fixImports {
-				// TODO: pass options to imports.Process so it
-				// can find symbols in sibling files.
-				out, err = imports.Process(f, in, nil)
-			} else {
-				var tmpDir string
-				tmpDir, err = os.MkdirTemp("", "gopformat")
-				if err != nil {
-					json.NewEncoder(w).Encode(fmtResponse{Error: err.Error()})
-					return
-				}
-				defer os.RemoveAll(tmpDir)
-				tmpGopFile := filepath.Join(tmpDir, "prog.gop")
-				if err = os.WriteFile(tmpGopFile, in, 0644); err != nil {
-					json.NewEncoder(w).Encode(fmtResponse{Error: err.Error()})
-					return
-				}
-				cmd := exec.Command("gop", "fmt", "-smart", tmpGopFile)
-				//gop fmt returns error result in stdout, so we do not need to handle stderr
-				//err is to check gop fmt return code
-				var fmtErr []byte
-				fmtErr, err = cmd.Output()
-				if err != nil {
-					json.NewEncoder(w).Encode(fmtResponse{Error: strings.Replace(string(fmtErr), tmpGopFile, "prog.gop", -1)})
-					return
-				}
-				out, err = os.ReadFile(tmpGopFile)
-				if err != nil {
-					err = errors.New("interval error when formatting gop code")
-				}
-			}
-			if err != nil {
-				errMsg := err.Error()
-				if !fixImports {
-					// Unlike imports.Process, format.Source does not prefix
-					// the error with the file path. So, do it ourselves here.
-					errMsg = fmt.Sprintf("%v:%v", f, errMsg)
-				}
-				json.NewEncoder(w).Encode(fmtResponse{Error: errMsg})
-				return
-			}
-			fs.AddFile(f, out)
-		case path.Base(f) == "go.mod":
-			out, err := formatGoMod(f, fs.Data(f))
-			if err != nil {
-				json.NewEncoder(w).Encode(fmtResponse{Error: err.Error()})
-				return
-			}
-			fs.AddFile(f, out)
+	json.NewEncoder(w).Encode(fmtResponse{Body: string(data)})
+	/*
+		fs, err := splitFiles([]byte(r.FormValue("body")))
+		if err != nil {
+			json.NewEncoder(w).Encode(fmtResponse{Error: err.Error()})
+			return
 		}
-	}
 
-	json.NewEncoder(w).Encode(fmtResponse{Body: string(fs.Format())})
+		fixImports := r.FormValue("imports") != ""
+		for _, f := range fs.files {
+			switch {
+			case path.Ext(f) == ".go":
+				var out []byte
+				var err error
+				in := fs.Data(f)
+				if fixImports {
+					// TODO: pass options to imports.Process so it
+					// can find symbols in sibling files.
+					out, err = imports.Process(f, in, nil)
+				} else {
+					var tmpDir string
+					tmpDir, err = os.MkdirTemp("", "gopformat")
+					if err != nil {
+						json.NewEncoder(w).Encode(fmtResponse{Error: err.Error()})
+						return
+					}
+					defer os.RemoveAll(tmpDir)
+					tmpGopFile := filepath.Join(tmpDir, "prog.gop")
+					if err = os.WriteFile(tmpGopFile, in, 0644); err != nil {
+						json.NewEncoder(w).Encode(fmtResponse{Error: err.Error()})
+						return
+					}
+					cmd := exec.Command("gop", "fmt", "-smart", tmpGopFile)
+					//gop fmt returns error result in stdout, so we do not need to handle stderr
+					//err is to check gop fmt return code
+					var fmtErr []byte
+					fmtErr, err = cmd.Output()
+					if err != nil {
+						json.NewEncoder(w).Encode(fmtResponse{Error: strings.Replace(string(fmtErr), tmpGopFile, "prog.gop", -1)})
+						return
+					}
+					out, err = os.ReadFile(tmpGopFile)
+					if err != nil {
+						err = errors.New("interval error when formatting gop code")
+					}
+				}
+				if err != nil {
+					errMsg := err.Error()
+					if !fixImports {
+						// Unlike imports.Process, format.Source does not prefix
+						// the error with the file path. So, do it ourselves here.
+						errMsg = fmt.Sprintf("%v:%v", f, errMsg)
+					}
+					json.NewEncoder(w).Encode(fmtResponse{Error: errMsg})
+					return
+				}
+				fs.AddFile(f, out)
+			case path.Base(f) == "go.mod":
+				out, err := formatGoMod(f, fs.Data(f))
+				if err != nil {
+					json.NewEncoder(w).Encode(fmtResponse{Error: err.Error()})
+					return
+				}
+				fs.AddFile(f, out)
+			}
+		}
+
+		json.NewEncoder(w).Encode(fmtResponse{Body: string(fs.Format())})
+	*/
 }
 
 func formatGoMod(file string, data []byte) ([]byte, error) {
@@ -106,4 +110,46 @@ func formatGoMod(file string, data []byte) ([]byte, error) {
 		return nil, err
 	}
 	return f.Format()
+}
+
+func formatXGoMod(file string, data []byte) ([]byte, error) {
+	f, err := xmodfile.Parse(file, data, nil)
+	if err != nil {
+		return nil, err
+	}
+	return modfile.Format(f.Syntax), nil
+}
+
+func formatCode(src []byte) ([]byte, error) {
+	ar, err := splitFiles(src)
+	if err != nil {
+		return nil, err
+	}
+	for _, file := range ar.files {
+		in := ar.Data(file)
+		var data []byte
+		var err error
+		switch path.Ext(file) {
+		case ".go":
+			data, err = format.Source(in)
+		case ".gop", ".xgo":
+			data, err = xformat.XGoStyleSource(in, false, file)
+		case ".gox":
+			data, err = xformat.XGoStyleSource(in, true, file)
+		case ".mod":
+			switch path.Base(file) {
+			case "go.mod":
+				data, err = formatGoMod(file, in)
+			case "xgo.mod":
+				data, err = formatXGoMod(file, in)
+			}
+		default:
+			data, err = xformat.XGoStyleSource(in, true, file)
+		}
+		if err != nil {
+			return nil, err
+		}
+		ar.AddFile(file, data)
+	}
+	return ar.Format(), nil
 }
